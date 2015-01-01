87ee8723a2f5b146bb24f268771d7202eed03003
Nihad Abbasov <narkoz.2008@gmail.com>
2015-01-01 14:26:10 +0400

    initial implementation

--- lib/destino/controllers/main.ex
+++ lib/destino/controllers/main.ex
@@ -5,7 +5,26 @@ defmodule Destino.Controllers.Main do
     if length(args) == 0 do
       render conn
     else
-      redirect conn, "https://rubygems.org/gems/#{args[:gem]}"
+      redirect conn, gem_page(args[:gem])
+    end
+  end
+
+  # Returns github page url or rubygems gem page url
+  # TODO: check if github page really exists
+  defp gem_page(gem) do
+    api_url = "https://rubygems.org/api/v1/gems/#{gem}.json"
+    json = HTTPoison.get!(api_url).body |> JSX.decode!
+    repo = get_repo_name(json["homepage_uri"]) || get_repo_name(json["source_code_uri"])
+
+    if is_nil(repo), do: json["project_uri"], else: "https://github.com/#{repo}"
+  end
+
+  # Extracts full repo name from github url
+  defp get_repo_name(url) do
+    unless is_nil(url) do
+      Regex.scan(~r/\Ahttps?:\/\/github.com\/(.+\/.+)/, url)
+      |> List.flatten
+      |> List.last
     end
   end
 end
