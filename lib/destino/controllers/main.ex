21a95533b9c0cbca3bde5f60872b6d39590aa2a1
Jun Lin <linjunpop@gmail.com>
2015-01-05 16:54:23 +0800

    Check github page exists.

--- lib/destino/controllers/main.ex
+++ lib/destino/controllers/main.ex
@@ -10,7 +10,6 @@ defmodule Destino.Controllers.Main do
   end
 
   # Returns github page url or rubygems gem page url
-  # TODO: check if github page really exists
   defp gem_page(gem) do
     api_url = "https://rubygems.org/api/v1/gems/#{gem}.json"
     response = HTTPoison.get! api_url
@@ -22,7 +21,11 @@ defmodule Destino.Controllers.Main do
       json = JSX.decode! response.body
       repo = get_repo_name(json["homepage_uri"]) || get_repo_name(json["source_code_uri"])
 
-      if is_nil(repo), do: json["project_uri"], else: "https://github.com/#{repo}"
+      if repo && github_exists?(repo) do
+        "https://github.com/#{repo}"
+      else
+        json["project_uri"]
+      end
     end
   end
 
@@ -32,6 +35,18 @@ defmodule Destino.Controllers.Main do
       Regex.scan(~r/\Ahttps?:\/\/github.com\/(.+\/.+)/, url)
       |> List.flatten
       |> List.last
+    end
+  end
+
+  defp github_exists?(repo) do
+    HTTPoison.start
+    case HTTPoison.head("https://github.com/#{repo}") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: _}} ->
+        true
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        false
+      {:error, %HTTPoison.Error{reason: _}} ->
+        false
     end
   end
 end
